5ae63562923fae0fccb5d48f5eba78764c5939a7
Mohammed <smith.mo.adam@gmail.com>
2024-08-09 00:18:42 +0400

    adding the web page to show the search result

--- esmaeel-essessment/src/main/java/com/esmaeel_essessment/controller/CarController.java
+++ esmaeel-essessment/src/main/java/com/esmaeel_essessment/controller/CarController.java
@@ -1,50 +1,60 @@
 package com.esmaeel_essessment.controller;
 
 
+
 import com.esmaeel_essessment.dto.CriteriasResponse;
 import com.esmaeel_essessment.model.Car;
 import com.esmaeel_essessment.service.CarService;
 import jakarta.xml.bind.JAXBException;
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
 
-@RestController
-//@Controller
-@RequestMapping("/api/cars")
-@Slf4j
+
+@Controller
+@RequestMapping("/cars")
 public class CarController {
 
     @Autowired
     private CarService carService;
 
-    @GetMapping
-    public CriteriasResponse criteriaOptions(){
-        return carService.criteriaOptions();
+    @GetMapping("/search-form")
+    public String criteriaOptions(Model model){
+        CriteriasResponse criteriasResponse = carService.criteriaOptions();
+        model.addAttribute("lengths", criteriasResponse.getLengths());
+        model.addAttribute("weights", criteriasResponse.getWeights());
+        model.addAttribute("velocities",criteriasResponse.getVelocities() );
+        model.addAttribute("colors", criteriasResponse.getColors());
+        return "search-form";
     }
 
     @PostMapping("/search")
-    public List<Car> searchCars(
+    public String searchCars(
             @RequestParam(required = false) Double length,
             @RequestParam(required = false) Double weight,
             @RequestParam(required = false) Double velocity,
-            @RequestParam(required = false) String color
+            @RequestParam(required = false) String color,Model model
             ) {
-
-
-        return carService.searchCars(length, weight, velocity, color);
+        List<Car> cars = carService.searchCars(length, weight, velocity, color);
+        model.addAttribute("cars", cars);
+        model.addAttribute("selectedLength", length);
+        model.addAttribute("selectedWeight", weight);
+        model.addAttribute("selectedVelocity", velocity);
+        model.addAttribute("selectedColor", color);
+        return "search-form";
     }
 
     @GetMapping("/download")
-    public ResponseEntity<String> downloadCarsAsXml(
+    public ResponseEntity<byte[]> downloadCarsAsXml(
             @RequestParam(required = false) Double length,
             @RequestParam(required = false) Double weight,
             @RequestParam(required = false) Double velocity,
@@ -56,7 +66,7 @@ public class CarController {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_XML);
         headers.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=cars.xml");
-
-        return new ResponseEntity<>(xmlContent, headers, HttpStatus.OK);
+        byte[] xmlBytes = xmlContent.getBytes();
+        return new ResponseEntity<>(xmlBytes, headers, HttpStatus.OK);
     }
 }
