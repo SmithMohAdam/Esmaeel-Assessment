021899397a3089d92172bcfc15729c2cee446058
Mohammed Adam Esmaeel <98095565+SmithMohAdam@users.noreply.github.com>
2024-08-10 15:44:40 +0400

    Update CarController.java

--- esmaeel-essessment/src/main/java/com/esmaeel_essessment/controller/CarController.java
+++ esmaeel-essessment/src/main/java/com/esmaeel_essessment/controller/CarController.java
@@ -16,6 +16,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/cars")
 public class CarController {
